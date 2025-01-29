b4ba0f4d1cd72dfad46cae2527fab5729113dcf6
mars2860 <anton.sysoev.ru68@gmail.com>
2025-01-29 10:58:42 +0300

    Поправил отступы

--- src/main.cpp
+++ src/main.cpp
@@ -40,61 +40,64 @@ void inputProcess()
 
   int t;
   cin>>t;   // Количество чисел во входном потоке данных
+  
   while(t--)
-    {
+  {
     int n;
     string x;
     cin>>n;  // Длина заданного числа
-        cin>>x;  // Заданное число
-        // Помещаем заданное число в очередь FIFO
-        inFifoMutex.lock();
-        inFifo.push(TernaryNumber(x));
-        inFifoMutex.unlock();
+    cin>>x;  // Заданное число
+    // Помещаем заданное число в очередь FIFO
+    inFifoMutex.lock();
+    inFifo.push(TernaryNumber(x));
+    inFifoMutex.unlock();
   }
+  
   isInputRun = false;
 }
 
 /// Поток, выполняющий поиск решения
 void solutionProcess()
 {
-    TernaryNumber x;
-    bool run = true;
-    bool dataAvailable = false;
-    while(run)
+  TernaryNumber x;
+  bool run = true;
+  bool dataAvailable = false;
+  
+  while(run)
+  {
+    // Забираем из входной очереди новое число
+    inFifoMutex.lock();
+    if(!inFifo.empty())
     {
-        // Забираем из входной очереди новое число
-        inFifoMutex.lock();
-        if(!inFifo.empty())
-        {
-            x = inFifo.front();
-            inFifo.pop();
-            dataAvailable = true;
-        }
-        inFifoMutex.unlock();
+      x = inFifo.front();
+      inFifo.pop();
+      dataAvailable = true;
+    }
+    inFifoMutex.unlock();
 
-        // Решаем задачу
-        if(dataAvailable)
-        {
-            TernaryNumber a(x.getLength()), b(x.getLength());
+    // Решаем задачу
+    if(dataAvailable)
+    {
+      TernaryNumber a(x.getLength()), b(x.getLength());
 
-            for(int i = 0; i < x.getLength(); ++i)
-            {
-                if(x.getDigit(i) == '1')
-                {
-                    a.pushDigit('1');
-                    b.pushDigit('0');
-                    for(int j = i + 1; j < x.getLength(); ++j)
-                    {
-                        b.pushDigit(x.getDigit(j));
-                    }
-                    break;
+      for(int i = 0; i < x.getLength(); ++i)
+      {
+        if(x.getDigit(i) == '1')
+        {
+          a.pushDigit('1');
+          b.pushDigit('0');
+          for(int j = i + 1; j < x.getLength(); ++j)
+          {
+            b.pushDigit(x.getDigit(j));
+          }
+          break;
         }
         else
-                {
-                    char digit = '0' + ((x.getDigit(i) - '0') / 2);
-                    a.pushDigit(digit);
-                    b.pushDigit(digit);
-                }
+        {
+          char digit = '0' + ((x.getDigit(i) - '0') / 2);
+          a.pushDigit(digit);
+          b.pushDigit(digit);
+        }
       }
 
       // Помещаем решение в выходную очередь
@@ -104,39 +107,40 @@ void solutionProcess()
       outFifoMutex.unlock();
     }
 
-        // Сбрасываем флаг наличия новых данных
-        dataAvailable = false;
+    // Сбрасываем флаг наличия новых данных
+    dataAvailable = false;
 
-        // Если входных данных больше не будет, то завершаем работу потока
-        inFifoMutex.lock();
-        if(!isInputRun && inFifo.empty())
-        {
-            run = false;
-        }
-        inFifoMutex.unlock();
+    // Если входных данных больше не будет, то завершаем работу потока
+    inFifoMutex.lock();
+    if(!isInputRun && inFifo.empty())
+    {
+      run = false;
     }
-    isSolutionRun = false;
+    inFifoMutex.unlock();
+  }
+
+  isSolutionRun = false;
 }
 
 /// Поток, выполняющий вывод результатов решения
 void outputProcess()
 {
-    bool run = true;
-    while(run)
+  bool run = true;
+  while(run)
+  {
+    outFifoMutex.lock();
+    while(!outFifo.empty())
     {
-        outFifoMutex.lock();
-        while(!outFifo.empty())
-        {
-            TernaryNumber x = outFifo.front();
-            outFifo.pop();
-            cout<<x.toString()<<endl;
-        }
-        if(!isSolutionRun)
-        {
-            run = false;
-        }
-    outFifoMutex.unlock();
+      TernaryNumber x = outFifo.front();
+      outFifo.pop();
+      cout<<x.toString()<<endl;
     }
+    if(!isSolutionRun)
+    {
+      run = false;
+    }
+    outFifoMutex.unlock();
+  }
 }
 
 int main(int argc, char** argv)
@@ -151,13 +155,13 @@ int main(int argc, char** argv)
     outFilename.append(argv[2]);
   }
   
-    thread thInput(inputProcess);
-    thread thSolution(solutionProcess);
-    thread thOutput(outputProcess);
+  thread thInput(inputProcess);
+  thread thSolution(solutionProcess);
+  thread thOutput(outputProcess);
 
-    thInput.join();
-    thSolution.join();
-    thOutput.join();
+  thInput.join();
+  thSolution.join();
+  thOutput.join();
 
-    return 0;
+  return 0;
 }
